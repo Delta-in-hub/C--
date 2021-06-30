602278eb072723c21d1ec980fa170a6f4e005be8
Delta-in-hub <tombrown796@gmail.com>
2021-06-30 23:31:57 +0800

    add vs sln

--- Readme.md
+++ Readme.md
@@ -1,6 +1,32 @@
 # ComplierTeam
 
+## 须知
+
+### 命名规范
+
+小驼峰命名
+
+`getNextToken()` 
+
+下划线命名
+
+`get_next_token()` 
+
+大驼峰命名
+
+`GetNextToken()`
+
+## code format
+
+vs里打开启用clang format
+
+
+
 ## 文法
 
 [见此文件](./Grammar Zoo - Browsable C99 Grammar.pdf) 
+
+https://github.com/rui314/9cc
+
+https://llvm.org/docs/tutorial/MyFirstLanguageFrontend/index.html
 
---
+++ src/stdafx.h
@@ -0,0 +1,25 @@
+#ifndef STDAFX
+#define STDAFX
+//包含通用的头文件
+
+#include <algorithm>
+#include <ciso646>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctype.h>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#endif
