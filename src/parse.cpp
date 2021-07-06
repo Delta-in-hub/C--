719fa62cce4a13f88904d0907572d126882153ad
mayflay <75985297+mayflay@users.noreply.github.com>
2021-07-06 09:30:50 +0800

    //

--- src/parse.cpp
+++ src/parse.cpp
@@ -243,11 +243,178 @@ Node* type_qualifier()
 /*
 assignment-expression::=
 conditional-expression
-unary-expression
-assignment-operator
-assignment-expression
+unary-expression assignment-operator assignment-expression
+*/
+Node* assignment_expression() {
+    
+    conditional_expression();
+}
+
+/*
+conditional-expression::=
+logical-OR-expression
+logical-OR-expression"?"expression":"conditional-expression
+*/
+Node* conditional_expression() {
+
+}
+/*
+logical-OR-expression::=
+logical-AND-expression
+logical-OR-expression"||"logical-AND-expression
 */
 
 
+Node* logical_OR_expression(){
+
+}
+
+/*
+logical-AND-expression::=
+inclusive-OR-expression
+logical-AND-expression"&&"inclusive-OR-expression
+*/
+
+Node* logical_AND_expression() {
+
+}
+/*
+inclusive-OR-expression::=
+exclusive-OR-expression
+inclusive-OR-expression"|"exclusive-OR-expression
+*/
+
+Node* inclusive_OR_expression() {
+
+}
+/*
+exclusive-OR-expression::=
+AND-expression
+exclusive-OR-expression"^"AND-expression
+*/
+
+Node* exclusive_OR_expression() {
+
+}
+/*
+AND-expression::=
+equality-expression
+AND-expression"&"equality-expression
+*/
+Node* AND_expression() {
+
+}
+/*
+equality-expression::=
+relational-expression
+equality-expression"=="relational-expression
+equality-expression"!="relational-expression
+*/
+
+Node* equality_expression() {
+
+}
+/*
+relational-expression::=
+shift-expression
+relational-expression"<"shift-expression
+relational-expression">"shift-expression
+relational-expression"<="shift-expression
+relational-expression">="shift-expression
+*/
+
+Node* relational_expression() {
+
+}
+
+/*
+shift-expression::=
+additive-expression
+shift-expression"<<"additive-expression
+shift-expression">>"additive-expression
+*/
+
+Node* shift_expression() {
+
+}
+
+/*
+additive-expression::=
+multiplicative-expression
+additive-expression"+"multiplicative-expression
+additive-expression"-"multiplicative-expression
+*/
+Node* additive_expression() {
+
+}
+/*
+multiplicative-expression::=
+cast-expression
+multiplicative-expression"*"cast-expression
+multiplicative-expression"/"cast-expression
+multiplicative-expression"%"cast-expression
+*/
+
+Node* multiplicative_expression(){
+
+}
+
+/*
+cast-expression::=
+unary-expression
+"("type-name")"cast-expression
+*/
+
+Node* cast_expression() {
+
+}
+/*
+unary-expression::=
+postfix-expression
+"++"unary-expression
+"--"unary-expression
+unary-operator cast-expression
+"sizeof"unary-expression
+"sizeof""("type-name")"
+*/
+
+Node* unary_expression() {
+
+}
+/*
+postfix-expression::=
+primary-expression
+postfix-expression"["expression"]"
+postfix-expression"("argument-expression-list?")"
+postfix-expression"."identifier
+postfix-expression"->"identifier
+postfix-expression"++"
+postfix-expression"--"
+"("type-name")""{"initializer-list"}"
+"("type-name")""{"initializer-list",""}"
+*/
+
+Node* postfix_expression() {
+
+}
+/*
+primary-expression::=
+identifier
+constant
+string_literal
+"("expression")"
+*/
+Node* primary_expression() {
+
+}
+/*
+expression::=
+assignment-expression
+expression","assignment-expression
+*/
+
+Node* expression() {
+
+}
 
 // [assignment_expression,expression] <-   武熠栋
